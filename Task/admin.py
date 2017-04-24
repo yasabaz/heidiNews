f10d36a5d9cba2a42567f82cb3f18c2bb43a290a
Mr.Illusion <mr.illusion@riseup.net>
2017-04-24 16:56:28 +0430

    uploader fix extentions and show title

--- Task/admin.py
+++ Task/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from models import Task
+from News.models import News
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display=('translator_id','news_id','time_translator','expiration_date')
+    list_display=('get_news_title','translator_id','news_id','time_translator','expiration_date')
 
-admin.site.register(Task,TaskAdmin)
+    def get_news_title(self,obj):
+        return obj.news_id.title
+
+admin.site.register(Task, TaskAdmin)
