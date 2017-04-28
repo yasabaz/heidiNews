0f63eb5026ccc3832ddbb009716ba741dcd6119c
Mr illusion <mrillusion@Mr>
2017-04-28 12:07:44 +0430

    add login page for Translator

--- Translator/views.py
+++ Translator/views.py
@@ -1,6 +1,27 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth import login as userlogin, logout as userlogout
+from urlparse import urlparse
+from django.contrib.auth.forms import AuthenticationForm
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+def index(request):
+	return render(request, 'index.html')
+
+def login(request):
+    if request.user.is_authenticated():
+        return HttpResponseRedirect(reverse("index"))
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            if user.is_active:
+                userlogin(request, user)
+                return HttpResponseRedirect('')
+    else:
+        form = AuthenticationForm()
+    return render(request, "login.html", {'form': form})
+
